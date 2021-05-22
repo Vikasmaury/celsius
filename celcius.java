02332c0055f7b5f32068b348e4a8eb060a6ca046
vikasmaurya <83597577+Vikasmaury@users.noreply.github.com>
2021-05-21 22:31:12 -0700

    Add files via upload

---
+++ celcius.java
@@ -0,0 +1,55 @@
+import java.util.*; 
+class Kelvin{
+
+  
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	float a;
+	float kelvin = 273;
+	
+	System.out.println("Temperature in Kelvin:");
+		a=sc.nextFloat();
+	 
+	   System.out.println("Temperature in celsius:");
+	   System.out.print(a/kelvin+" C ");
+	
+	   
+
+	    		
+}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Declare the reader from console
+// 		int br;
+// 		// Get the console input for the temperature in Kelvin
+// 		System.out.println("Temperature in Kelvin:");		
+		
+// 		// assign to float variable the temperature in Kelvin
+// 		float kelvin = Float.parseFloat(br.readLine());
+// 		// Kelvin to Degree Celsius Conversion
+// 		float celsius = kelvin - 273.15F;
+// 		System.out.println("Celsius: "+ celsius);
